fixed textures for the palace obj, added other results, fixed the cube scaling

main.py
@@ -1,15 +1,14 @@
 import os
 import time
 import torch
-import torch_directml
 from datetime import timedelta
-from torchvision import transforms, datasets
-import torch.nn as nn
+from torchvision import transforms
 from PIL import Image, ImageOps
-
-from pytorch3d.utils import ico_sphere
 import numpy as np
 from tqdm import tqdm
+from pathlib import Path
+
+from pytorch3d.utils import ico_sphere
 from pytorch3d.structures.meshes import Meshes
 from pytorch3d.io import IO, save_obj
 from pytorch3d.io import load_objs_as_meshes
@@ -17,7 +16,6 @@ from pytorch3d.renderer import PointLights, look_at_view_transform, FoVPerspecti
     MeshRenderer, MeshRasterizer, SoftSilhouetteShader
 from pytorch3d.loss import mesh_edge_loss, mesh_laplacian_smoothing, mesh_normal_consistency
 from pytorch3d.ops import SubdivideMeshes
-from pathlib import Path
 
 base_dir = Path().resolve()
 
@@ -34,13 +32,14 @@ class FitMesh:
         self.starting_shape = starting_shape
         self.optimized_mesh = Meshes(verts=[], faces=[])
 
-    def train_on_mesh(self):
+    def construct_from_mesh(self):
         # scale mesh
         verts = self.mesh.verts_packed()
         center = verts.mean(0)
         scale = max((verts - center).abs().max(0)[0])
         self.mesh.offset_verts_(-center)
-        self.mesh.scale_verts_((1.0 / float(scale)))
+        # scale the vertices so that the mesh fits within the cube of side length 1
+        self.mesh.scale_verts_(0.5 / float(scale))
 
         # duplicate meshes
         mesh_list = self.mesh.extend(self.number_of_views)
@@ -55,7 +54,7 @@ class FitMesh:
 
         silhouette_images = self.renderer(mesh_list, cameras=cameras, lights=lights)
 
-        # silhouettes_cow is a 4D tensor (batch_size, height, width, channels).
+        # silhouettes_images is a 4D tensor (batch_size, height, width, channels).
         # index 3 is the alpha channel. we extract only the alpha channel
         target_silhouette = []
         for i in range(self.number_of_views):
@@ -79,10 +78,9 @@ class FitMesh:
 
         verts_shape = source_mesh.verts_packed().shape
         deform_verts = torch.full(verts_shape, 0.0, device=self.device, requires_grad=True)
-
-        optimizer = torch.optim.SGD([deform_verts], lr=0.25, momentum=0.9)
         new_src_mesh = source_mesh.offset_verts(deform_verts)
 
+        optimizer = torch.optim.SGD([deform_verts], lr=0.25, momentum=0.9)
         loop = tqdm(range(self.iterations))
 
         for i in loop:
@@ -95,7 +93,7 @@ class FitMesh:
 
             update_mesh_shape_prior_losses(new_src_mesh, loss)
 
-            for j in np.random.permutation(self.number_of_views).tolist()[:num_views_per_iteration]:
+            for j in np.random.permutation(self.number_of_views).tolist()[:2]:
                 # render 2 random images and calculate loss
                 images_predicted = self.renderer(new_src_mesh, cameras=target_cameras[j], lights=lights)
                 predicted_silhouette = images_predicted[..., 3]  # 3 = alpha channel
@@ -106,9 +104,9 @@ class FitMesh:
             sum_loss = torch.tensor(0.0, device=self.device)
             for k, l in loss.items():
                 sum_loss += l * losses[k]["weight"]
-                losses[k]["values"].append(float(l.detach().cpu()))
+                losses[k]["values"].append(float(l.detach()))
 
-            loop.set_description(f"total_loss_train = {sum_loss:.6f}")
+            loop.set_description(f"total_loss_mesh = {sum_loss:.6f}")
 
             sum_loss.backward()
             optimizer.step()
@@ -120,7 +118,7 @@ class FitMesh:
         final_obj = os.path.join('./', 'final_model.obj')
         save_obj(final_obj, final_verts, final_faces)
 
-    def construct_mesh(self, images_path, angles, iterations):
+    def construct_from_images(self, target_silhouette, angles, iterations):
         verts = self.mesh.verts_packed()
         center = verts.mean(0)
         scale = max((verts - center).abs().max(0)[0])
@@ -130,7 +128,59 @@ class FitMesh:
         if self.optimized_mesh.verts_packed().numel() == 0 and self.optimized_mesh.faces_packed().numel() == 0:
             self.optimized_mesh = load_objs_as_meshes([optimized_path], device=self.device)
 
-        """ load images """
+        """ configure losses """
+        losses = {
+            "silhouette": {"weight": 1.0, "values": []},
+            "edge": {"weight": 1.0, "values": []},
+            "normal": {"weight": 0.01, "values": []},
+            "laplacian": {"weight": 1.0, "values": []},
+        }
+
+        verts_shape = self.optimized_mesh.verts_packed().shape
+        deform_verts = torch.full(verts_shape, 0.0, device=self.device, requires_grad=True)
+
+        optimizer = torch.optim.SGD([deform_verts], lr=0.12, momentum=0.9)
+        new_src_mesh = self.optimized_mesh.offset_verts(deform_verts)
+
+        loop = tqdm(range(iterations))
+
+        for i in loop:
+            optimizer.zero_grad()
+            new_src_mesh = self.optimized_mesh.offset_verts(deform_verts)
+
+            loss = {}
+            for k in losses:
+                loss[k] = torch.tensor(0.0, device=self.device)
+
+            update_mesh_shape_prior_losses(new_src_mesh, loss)
+
+            for j in np.random.permutation(len(silhouettes)).tolist()[:iterations]:
+                # render 2 random images and calculate loss
+                R, T = look_at_view_transform(dist=2.7, elev=angles[j][0], azim=angles[j][1])
+                cameras = FoVPerspectiveCameras(device=self.device, R=R, T=T)
+                images_predicted = self.renderer(new_src_mesh, cameras=cameras, lights=lights)
+                predicted_silhouette = images_predicted[..., 3]
+                loss_silhouette = ((predicted_silhouette - target_silhouette[j]) ** 2).mean()
+                loss["silhouette"] += loss_silhouette / len(silhouettes)
+
+            # Weighted sum of the losses
+            sum_loss = torch.tensor(0.0, device=self.device)
+            for k, l in loss.items():
+                sum_loss += l * losses[k]["weight"]
+                losses[k]["values"].append(float(l.detach()))
+
+            loop.set_description(f"total_loss_images = {sum_loss:.6f}")
+
+            sum_loss.backward()
+            optimizer.step()
+
+        final_verts, final_faces = new_src_mesh.get_mesh_verts_faces(0)
+        final_verts = final_verts * scale + center
+
+        final_obj = os.path.join('./', 'new_model.obj')
+        save_obj(final_obj, final_verts, final_faces)
+
+    def load_images(self, images_path):
         image_list = []
         transform = transforms.Compose([transforms.ToTensor()])
         for file in images_path.iterdir():
@@ -148,66 +198,13 @@ class FitMesh:
         target_silhouette = []
         for i in range(silhouette_images.shape[0]):
             target_silhouette.append(silhouette_images[i, ..., 3])
-
-        # configure losses
-        losses = {
-            "silhouette": {"weight": 1.0, "values": []},
-            "edge": {"weight": 1.0, "values": []},
-            "normal": {"weight": 0.01, "values": []},
-            "laplacian": {"weight": 1.0, "values": []},
-        }
-
-        verts_shape = self.optimized_mesh.verts_packed().shape
-        deform_verts = torch.full(verts_shape, 0.0, device=self.device, requires_grad=True)
-
-        optimizer = torch.optim.SGD([deform_verts], lr=0.12, momentum=0.9)
-        new_src_mesh = self.optimized_mesh.offset_verts(deform_verts)
-
-        loop = tqdm(range(self.iterations))
-
-        for i in loop:
-            optimizer.zero_grad()
-            new_src_mesh = self.optimized_mesh.offset_verts(deform_verts)
-
-            loss = {}
-            for k in losses:
-                loss[k] = torch.tensor(0.0, device=self.device)
-
-            update_mesh_shape_prior_losses(new_src_mesh, loss)
-
-            for j in np.random.permutation(len(image_list)).tolist()[:iterations]:
-                # render 2 random images and calculate loss
-                R, T = look_at_view_transform(dist=2.7, elev=angles[j][0], azim=angles[j][1])
-                cameras = FoVPerspectiveCameras(device=self.device, R=R, T=T)
-                images_predicted = self.renderer(new_src_mesh, cameras=cameras, lights=lights)
-                predicted_silhouette = images_predicted[..., 3]
-                loss_silhouette = ((predicted_silhouette - target_silhouette[j]) ** 2).mean()
-                loss["silhouette"] += loss_silhouette / len(image_list)
-
-            # Weighted sum of the losses
-            sum_loss = torch.tensor(0.0, device=self.device)
-            for k, l in loss.items():
-                sum_loss += l * losses[k]["weight"]
-                losses[k]["values"].append(float(l.detach()))
-
-            loop.set_description(f"total_loss_construct = {sum_loss:.6f}")
-
-            sum_loss.backward()
-            optimizer.step()
-
-        final_verts, final_faces = new_src_mesh.get_mesh_verts_faces(0)
-        final_verts = final_verts * scale + center
-
-        final_obj = os.path.join('./', 'new_model.obj')
-        save_obj(final_obj, final_verts, final_faces)
+        return target_silhouette
 
 
 # Losses to smooth / regularize the mesh shape
 def update_mesh_shape_prior_losses(mesh, loss):
     loss["edge"] = mesh_edge_loss(mesh)
-
     loss["normal"] = mesh_normal_consistency(mesh)
-
     loss["laplacian"] = mesh_laplacian_smoothing(mesh, method="uniform")
 
 
@@ -227,16 +224,16 @@ def pad_to_square(image):
 
 
 def create_cube(level, device):
-    # create a cube with sid length = 1
+    # create a cube with side length = 0.5
     vertices = torch.tensor([
-        [-1, -1, -1],  # 0
-        [-1, -1, 1],  # 1
-        [-1, 1, -1],  # 2
-        [-1, 1, 1],  # 3
-        [1, -1, -1],  # 4
-        [1, -1, 1],  # 5
-        [1, 1, -1],  # 6
-        [1, 1, 1]  # 7
+        [-0.5, -0.5, -0.5],  # 0
+        [-0.5, -0.5, 0.5],  # 1
+        [-0.5, 0.5, -0.5],  # 2
+        [-0.5, 0.5, 0.5],  # 3
+        [0.5, -0.5, -0.5],  # 4
+        [0.5, -0.5, 0.5],  # 5
+        [0.5, 0.5, -0.5],  # 6
+        [0.5, 0.5, 0.5]  # 7
     ], dtype=torch.float32, device=device)
 
     # faces of the cube. using counter-clockwise order so normals direct outwards
@@ -259,20 +256,21 @@ def create_cube(level, device):
 if __name__ == "__main__":
     start_time = time.perf_counter()
 
-    obj_path_3d = base_dir / "metropolitan_palace.obj"
-
     device_cpu = torch.device("cpu")
-    device_gpu = torch_directml.device()
-    device_gpu_conda = torch.device("cuda:0")
-
+    device_gpu_cuda = torch.device("cuda:0")
     # choose device
-    device = device_gpu_conda
+    device = device_cpu
 
     # initialize elevation and azimuth of cameras
     num_of_views = 20
     elevation = torch.linspace(0, 360, num_of_views, device=device)
     azimuth = torch.linspace(-180, 180, num_of_views, device=device)
 
+    # estimated camera angles for the building
+    building_angles = [(0, -180), (0, -160), (0, -130), (0, -110), (0, -90), (0, -60), (0, -36), (0, -15),
+                       (0, 15), (0, 36), (0, 60), (0, 90), (0, 110), (0, 130), (0, 160), (0, 180)]
+
+    cow_angles = list(zip(elevation.tolist(), azimuth.tolist()))
     # create lights
     lights = PointLights(device=device, location=[[0.0, 0.0, -3.0]])
 
@@ -280,32 +278,40 @@ if __name__ == "__main__":
     R, T = look_at_view_transform(dist=2.7, elev=elevation, azim=azimuth, device=device)
     cameras = FoVPerspectiveCameras(device=device, R=R, T=T)
 
-    '''silhouette renderer '''
-    #  Create silhouettes rasterization + renderer
+    """ silhouette renderer """
+    #  create silhouettes rasterization + renderer
     raster_settings_silhouette = RasterizationSettings(image_size=128, blur_radius=np.log(1. / 1e-4 - 1.) * 1e-4,
                                                        faces_per_pixel=50)
     renderer_silhouette = MeshRenderer(
         rasterizer=MeshRasterizer(cameras=cameras, raster_settings=raster_settings_silhouette),
         shader=SoftSilhouetteShader())
 
-    fit_mesh = FitMesh(obj_path_3d, renderer_silhouette, device, num_of_views, 3000, 'sphere')
-    # train model
-    fit_mesh.train_on_mesh()
+    obj_path_3d = base_dir / "metropolitan_palace.obj"
+    fit_mesh = FitMesh(obj_path_3d, renderer_silhouette, device, num_of_views, 3000, 'cube')
 
+    """ construct from mesh """
+    # fit_mesh.construct_from_mesh()
+
+    """ construct from images """
     images = base_dir / "silhouettes_my_building"
-
-    building_angles = [(0, -180), (0, -160), (0, -130), (0, -110), (0, -90), (0, -60), (0, -36), (0, -15),
-                       (0, 15), (0, 36), (0, 60), (0, 90), (0, 110), (0, 130), (0, 160), (0, 180)]
-
-    cow_angles = [(0.0, -180.0), (18.9474, -161.0526), (37.8947, -142.1053), (56.8421, -123.1579), (75.7895, -104.2105),
-                  (94.7368, -85.2632), (113.6842, -66.3158), (132.6316, -47.3684), (151.5789, -28.4211),
-                  (170.5263, -9.4737), (189.4737, 9.4737), (208.4211, 28.4211), (227.3684, 47.3684),
-                  (246.3158, 66.3158), (265.2632, 85.2632), (284.2105, 104.2105), (303.1579, 123.1579),
-                  (322.1053, 142.1053), (341.0526, 161.0526), (360.0, 180.0)]
-
-    # use model
-    fit_mesh.construct_mesh(images, cow_angles, 3000)
+    silhouettes = fit_mesh.load_images(images)
+    fit_mesh.construct_from_images(silhouettes, building_angles, 100)
 
     end_time = time.perf_counter()
     elapsed = end_time - start_time
     print("Elapsed time: ", str(timedelta(seconds=elapsed)))
+
+# saving and loading
+# PATH_OPT = "optimizer.pt"
+# torch.save(optimizer.state_dict(), PATH_OPT)
+# PATH = "model.pt"
+# torch.save(new_src_mesh.verts_packed(), PATH)
+# PATH_REND = "renderer.pt"
+# torch.save(renderer_silhouette, PATH_REND)
+#
+# optimizer_loaded = torch.load(PATH_OPT)
+# new_src_mesh_loaded = torch.load(PATH)
+# renderer_silhouette_loaded = torch.load(PATH_REND)
+#
+# new = renderer_silhouette.rasterizer
+